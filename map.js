64e202902b388de07f768e56f4bd1a1b8d43fc72
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-09-19 21:05:52 -0400

    added remainder of map requirements

--- map.js
+++ map.js
@@ -1,12 +1,20 @@
 //create map
+var map = L.map('map').setView([42.842151, -78.800499]);
 
-
-
-
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
 //get user location
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
-
+console.log(getCoords());
 
 
 
